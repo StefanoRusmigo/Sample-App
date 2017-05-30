28698d1d902393e39722168fb98a21b4e45a2bb5
StefanoRusmigo <stefanorusmigo@hotmail.com>
2017-05-30 19:03:36 +0300

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,6 +26,8 @@ test "login with valid information and then logout" do
   assert_select "a[href=?]",user_path(@user)
   delete logout_path
   assert_redirected_to root_path
+   # Simulate a user clicking logout in a second window.
+    delete logout_path
   follow_redirect!
   assert_not is_logged_in?
   assert_select "a[href=?]",login_path
@@ -34,6 +36,19 @@ test "login with valid information and then logout" do
 
 end
 
+test 'login with remembering' do 
+  log_in_as(@user)
+  assert_not_empty cookies['remember_token']
+  assert_equal @controller.send(:cookies).signed[:remember_token], assigns(:user).remember_token # signed method doesnt work on test cookies beacause in testing env cookies are just hashes and not instances of cookies class.  This code converts it to cookies so we can use signed method
+end
+
+test 'login without remembering' do 
+  # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user,remember_me:'0')
+    assert_empty cookies['remember_token']
+end
 
 
 end
